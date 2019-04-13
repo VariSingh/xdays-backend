b12d45e16510e24baf2e90ef44dcd899c18b12d6
Varinder Singh <varindersingh271990@gmail.com>
2019-04-13 19:19:41 +0530

    changed get days response format

--- controllers/day.controller.js
+++ controllers/day.controller.js
@@ -9,12 +9,16 @@ exports.findAll = (req, res, next) => {
         if (!result) {
             res.status(200).send("Challenge not found");
         } else {
-
-            Day.find({ "challenge": req.params.challengeId }, (error, result) => {
+                const challengeDetails = result;
+            Day.find({ "challenge": req.params.challengeId },{__v:0},{sort:{date: -1}}, (error, result) => {
+                const response = {
+                    daysList:result,
+                    challengeDetails:challengeDetails
+                }
                 if (error) {
                     res.status(400).send(error);
                 } else {
-                    res.status(200).send(result);
+                    res.status(200).send(response);
                 }
 
             })
